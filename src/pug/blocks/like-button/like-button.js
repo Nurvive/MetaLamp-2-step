e0b9615fba740fa7836a6784a1df55fb35f9fb03
Nurvive <jiimbo689@gmail.com>
2021-07-14 21:17:03 +0700

    Добавил like

---
+++ src/pug/blocks/like-button/like-button.js
@@ -0,0 +1,16 @@
+import './like-button.pug'
+import './like-button-kit/like-button.scss'
+
+$().ready(()=>{
+    $('.like-button').on('click',function (){
+        if($(this).hasClass('like-button_active')){
+            $(this).removeClass('like-button_active')
+            $(this).children('.like-button__count').text($(this).text() - 1)
+        }else{
+            $(this).addClass('like-button_active')
+            $(this).children('.like-button__count').text(Number($(this).text()) + 1)
+        }
+
+
+    })
+})
