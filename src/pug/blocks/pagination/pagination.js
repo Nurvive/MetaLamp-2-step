f047b7cc77c4750924cd0a075060d126454935d3
Nurvive <jiimbo689@gmail.com>
2021-07-20 12:53:51 +0700

    Добавил pagination

--- src/pug/blocks/pagination/pagination.js
+++ src/pug/blocks/pagination/pagination.js
@@ -1,3 +1,21 @@
 import './pagination.pug'
 import './pagination-kit/pagination.scss'
-let countOnPage = 12;
+import 'paginationjs'
+
+$().ready(() => {
+    $('.pagination').pagination({
+        dataSource: function (done) {
+            let result = [];
+            for (let i = 1; i < 180; i++) {
+                result.push(i);
+            }
+            done(result);
+        },
+        pageSize: 12,
+        pageRange: 1,
+        autoHideNext: true,
+        autoHidePrevious: true,
+        prevText: `<div class='pagination__arrow-prev'></div>`,
+        nextText: `<div class='pagination__arrow-next'></div>`
+    })
+})
