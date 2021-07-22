bc87c04b952c47c0170ea63093581a85f487f965
Nurvive <jiimbo689@gmail.com>
2021-07-22 16:22:01 +0700

    Добавил окно бронирования

--- src/pug/blocks/reservation/reservation.js
+++ src/pug/blocks/reservation/reservation.js
@@ -1,1 +1,21 @@
 import './reservation-kit/reservation.scss'
+
+$().ready(function () {
+    let $start = $('#reservation__date-dropdown_start'),
+        $end = $('#reservation__date-dropdown_end');
+
+    $start.datepicker({
+        onSelect: function (fd, date) {
+            $end.data('datepicker')
+                .update('minDate', date);
+
+            $end.focus();
+        }
+    })
+    $end.datepicker({
+        onSelect: function (fd, date) {
+            $start.data('datepicker')
+                .update('maxDate', date)
+        }
+    })
+})
