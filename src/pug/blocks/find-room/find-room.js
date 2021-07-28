0b293c020dba79c4ab222da6e5ba69bae3102c02
Nurvive <jiimbo689@gmail.com>
2021-07-28 16:53:50 +0700

    pixel perfect на странице landing

--- src/pug/blocks/find-room/find-room.js
+++ src/pug/blocks/find-room/find-room.js
@@ -6,10 +6,15 @@ $().ready(function () {
     let $start = $('#find-room__date-dropdown_start'),
         $end = $('#find-room__date-dropdown_end');
 
-    $start.datepicker({
+    let pick = $start.datepicker({
         onSelect: function (fd, d, picker) {
             $start.val(fd.split("-")[0]);
             $end.val(fd.split("-")[1]);
         }
-    })
+    }).data('datepicker')
+    let pckr = $('.datepickers-container .datepicker')
+    pckr.append('<div class="text-buttons">' +
+        '<div class="text-button"><div class="text-button__text">Очистить</div></div>' +
+        '<div class="text-button"><div class="text-button__text">Применить</div></div>' +
+        '</div>')
 })
