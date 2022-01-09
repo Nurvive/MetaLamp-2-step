f933ce1d39aed4e6ffe514fd254819175285a040
Nurvive <jiimbo689@gmail.com>
2022-01-09 20:23:23 +0700

    Рефакторинг

--- src/pug/blocks/date-dropdown/date-dropdown.js
+++ src/pug/blocks/date-dropdown/date-dropdown.js
@@ -1,14 +1,15 @@
 import './date-dropdown-kit/date-dropdown.scss';
 import 'air-datepicker/dist/css/datepicker.min.css';
 import 'air-datepicker';
+
 $().ready(function () {
-    $('.date-dropdown-input').datepicker({
+    $('.js-date-dropdown-input').datepicker({
         classes: 'toggle-datepicker',
-        onShow: function () {
-            $(this).addClass('date-dropdown_active');
+        onShow: function (e) {
+            $(e.el.parentNode).addClass('date-dropdown_active');
         },
-        onHide: function () {
-            $(this).removeClass('date-dropdown_active');
+        onHide: function (e) {
+            $(e.el.parentNode).removeClass('date-dropdown_active');
         },
         position: 'bottom center'
     });
