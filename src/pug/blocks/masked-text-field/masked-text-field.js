5633c0396e5d86716e0bc6f5047b793a9430ba3e
Nurvive <jiimbo689@gmail.com>
2022-01-09 22:39:21 +0700

    Обработал линтером

--- src/pug/blocks/masked-text-field/masked-text-field.js
+++ src/pug/blocks/masked-text-field/masked-text-field.js
@@ -1,18 +1,25 @@
-import './masked-text-field-kit/masked-text-field.scss'
+import './masked-text-field-kit/masked-text-field.scss';
 
 import IMask from 'imask';
 
 $().ready(function () {
-    if (document.querySelector("#masked-text-field") !== null) {
+    if (document.querySelector('#masked-text-field') !== null) {
         IMask(
             document.getElementById('masked-text-field'),
             {
                 mask: Date,
                 blocks: {
-                    d: {mask: IMask.MaskedRange, placeholderChar: 'ДД', from: 1, to: 31, maxLength: 2},
-                    m: {mask: IMask.MaskedRange, placeholderChar: 'ММ', from: 1, to: 12, maxLength: 2},
-                    Y: {mask: IMask.MaskedRange, placeholderChar: 'ГГГГ', from: 1900, to: 2999, maxLength: 4}
+                    d: {
+                        mask: IMask.MaskedRange, placeholderChar: 'ДД', from: 1, to: 31, maxLength: 2
+                    },
+                    m: {
+                        mask: IMask.MaskedRange, placeholderChar: 'ММ', from: 1, to: 12, maxLength: 2
+                    },
+                    Y: {
+                        mask: IMask.MaskedRange, placeholderChar: 'ГГГГ', from: 1900, to: 2999, maxLength: 4
+                    }
                 }
-            });
+            }
+        );
     }
-})
+});
