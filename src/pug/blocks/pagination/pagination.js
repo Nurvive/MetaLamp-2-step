1040014737eddedcab2c6ebe1fd2f547ac56e1c4
Nurvive <jiimbo689@gmail.com>
2022-01-09 22:44:06 +0700

    Обработал линтером

--- src/pug/blocks/pagination/pagination.js
+++ src/pug/blocks/pagination/pagination.js
@@ -1,13 +1,12 @@
-
-import './pagination-kit/pagination.scss'
-import 'paginationjs'
+import './pagination-kit/pagination.scss';
+import 'paginationjs';
 
 $().ready(() => {
-    if (document.querySelector(".pagination") !== null) {
+    if (document.querySelector('.pagination') !== null) {
         $('.pagination').pagination({
             dataSource: function (done) {
                 let result = [];
-                for (let i = 1; i < 180; i++) {
+                for (let i = 1; i < 180; i += 1) {
                     result.push(i);
                 }
                 done(result);
@@ -16,14 +15,13 @@ $().ready(() => {
             pageRange: 1,
             autoHideNext: true,
             autoHidePrevious: true,
-            prevText: `<div class='pagination__arrow-prev'></div>`,
-            nextText: `<div class='pagination__arrow-next'></div>`,
-            callback:function(data, pagination) {
+            prevText: '<div class=\'pagination__arrow-prev\'></div>',
+            nextText: '<div class=\'pagination__arrow-next\'></div>',
+            callback: function (data, pagination) {
                 $('.pagination__info1').text(data[0]);
                 $('.pagination__info2').text(data[data.length - 1]);
                 $('.pagination__info3').text(pagination.totalNumber);
-
             }
-        })
+        });
     }
-})
+});
