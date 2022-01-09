06effc9b7cc8cbb54d3111175e88610bfd4d3de9
Nurvive <jiimbo689@gmail.com>
2022-01-09 22:48:20 +0700

    Обработал линтером и отрефакторил

--- src/pug/blocks/range-slider/range-slider.js
+++ src/pug/blocks/range-slider/range-slider.js
@@ -1,22 +1,21 @@
+import './range-slider-kit/range-slider.scss';
+import 'ion-rangeslider';
+import 'ion-rangeslider/css/ion.rangeSlider.min.css';
 
-import './range-slider-kit/range-slider.scss'
-
-import 'ion-rangeslider'
-import 'ion-rangeslider/css/ion.rangeSlider.min.css'
-$(".range-slider__slider").ionRangeSlider({
-    skin: "round",
+$('.range-slider__slider').ionRangeSlider({
+    skin: 'round',
     min: 0,
-    type:'double',
+    type: 'double',
     max: 10000,
     from: 2000,
     to: 5000,
-    step:20,
+    step: 20,
     hide_min_max: true,
     hide_from_to: true,
     onStart: function (data) {
-        $('.range-slider__count').text(this.from +'₽'+ ' - ' +this.to+'₽')
+        $('.range-slider__count').text(`${data.from } ₽ - ${data.to} ₽`);
     },
-    onChange: function (data){
-        $('.range-slider__count').text(data.from+'₽' + ' - ' +data.to+'₽')
+    onChange: function (data) {
+        $('.range-slider__count').text(`${data.from } ₽ - ${data.to} ₽`);
     }
 });
