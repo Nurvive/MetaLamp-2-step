17e947a8eff0f0262743937e4f532e876453056b
Nurvive <jiimbo689@gmail.com>
2022-01-09 16:21:54 +0700

    Обработал линтером main.js

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,6 @@
-import "../scss/index.scss"
+import '../scss/index.scss';
 function importAll(resolve) {
     resolve.keys().forEach(resolve);
 }
 
-importAll(require.context('../pug/', true,/\.js$/))
+importAll(require.context('../pug/', true, /\.js$/));
