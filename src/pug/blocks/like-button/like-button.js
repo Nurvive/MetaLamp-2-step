24baea99bbb88778e9bef544d77ec62976e503fb
Nurvive <jiimbo689@gmail.com>
2022-01-09 22:36:52 +0700

    Обработал линтером и отрефакторил

--- src/pug/blocks/like-button/like-button.js
+++ src/pug/blocks/like-button/like-button.js
@@ -1,16 +1,13 @@
-
-import './like-button-kit/like-button.scss'
+import './like-button-kit/like-button.scss';
 
 $().ready(()=>{
-    $('.like-button').on('click',function (){
-        if($(this).hasClass('like-button_active')){
-            $(this).removeClass('like-button_active')
-            $(this).children('.like-button__count').text($(this).text() - 1)
-        }else{
-            $(this).addClass('like-button_active')
-            $(this).children('.like-button__count').text(Number($(this).text()) + 1)
+    $('.like-button').on('click', function () {
+        if ($(this).hasClass('like-button_active')) {
+            $(this).removeClass('like-button_active');
+            $(this).children('.like-button__count').text($(this).text() - 1);
+        } else {
+            $(this).addClass('like-button_active');
+            $(this).children('.like-button__count').text(Number($(this).text()) + 1);
         }
-
-
-    })
-})
+    });
+});
