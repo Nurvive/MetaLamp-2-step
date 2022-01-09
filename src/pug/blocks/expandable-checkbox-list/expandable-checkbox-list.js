f36d9a382f16b00ce22d2d2282f1e4a8a2e4b5f0
Nurvive <jiimbo689@gmail.com>
2022-01-09 22:00:51 +0700

    Обработал линтером и отрефакторил

--- src/pug/blocks/expandable-checkbox-list/expandable-checkbox-list.js
+++ src/pug/blocks/expandable-checkbox-list/expandable-checkbox-list.js
@@ -1,33 +1,28 @@
+import './expandable-checkbox-list-kit/expandable-checkbox-list.scss';
 
-import './expandable-checkbox-list-kit/expandable-checkbox-list.scss'
 function ReverseObject(Obj) {
     let NewObj = [];
-    for (let item of Obj) {
+    $.each(Obj, (index, item) => {
         NewObj.unshift(item);
-    }
+    });
     return NewObj;
 }
+
 $().ready(() => {
     $('.expandable-checkbox__open-button').on('click', function () {
-        let list = $('.expandable-checkbox__list')
-        $(this).toggleClass('expandable-checkbox__open-button_active')
-        let items = list.children()
+        let list = $('.expandable-checkbox__list');
+        $(this).toggleClass('expandable-checkbox__open-button_active');
+        let items = list.children();
         if (list.hasClass('expandable-checkbox__list_active')) {
-            items  = ReverseObject(items)
-            for (let item of items){
-                setTimeout(()=>item.classList.remove('show'), 100, item)
-            }
+            items = ReverseObject(items);
+            $.each(items, (index, item) => {
+                setTimeout(() => item.classList.remove('show'), 100, item);
+            });
         } else {
-            for (let item of items) {
-                setTimeout(() => item.classList.add('show'), 100, item)
-            }
+            $.each(items, (index, item) => {
+                setTimeout(() => item.classList.add('show'), 100, item);
+            });
         }
-        list.toggleClass('expandable-checkbox__list_active')
-
-
-    })
-
-
-
-})
-
+        list.toggleClass('expandable-checkbox__list_active');
+    });
+});
