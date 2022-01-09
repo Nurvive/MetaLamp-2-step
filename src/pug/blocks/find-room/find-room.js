f4feafb37078fcfeb6f8341f61c4f5533c11428a
Nurvive <jiimbo689@gmail.com>
2022-01-09 22:09:49 +0700

    Обработал линтером и отрефакторил

--- src/pug/blocks/find-room/find-room.js
+++ src/pug/blocks/find-room/find-room.js
@@ -1,20 +1,20 @@
-import './find-room-kit/find-room.scss'
-import 'air-datepicker'
+import './find-room-kit/find-room.scss';
+import 'air-datepicker';
 import 'air-datepicker/dist/css/datepicker.min.css';
 
 $().ready(function () {
-    let $start = $('#find-room__date-dropdown_start'),
-        $end = $('#find-room__date-dropdown_end');
+    let $start = $('#find-room__date-dropdown_start');
+    let $end = $('#find-room__date-dropdown_end');
 
-    let pick = $start.datepicker({
-        onSelect: function (fd, d, picker) {
-            $start.val(fd.split("-")[0]);
-            $end.val(fd.split("-")[1]);
+    $start.datepicker({
+        onSelect: function (fd) {
+            $start.val(fd.split('-')[0]);
+            $end.val(fd.split('-')[1]);
         }
-    }).data('datepicker')
-    let pckr = $('.datepickers-container .datepicker')
-    pckr.append('<div class="text-buttons">' +
-        '<div class="text-button"><div class="text-button__text">Очистить</div></div>' +
-        '<div class="text-button"><div class="text-button__text">Применить</div></div>' +
-        '</div>')
-})
+    }).data('datepicker');
+    let pckr = $('.datepickers-container .datepicker');
+    pckr.append('<div class="text-buttons">'
+        + '<div class="text-button"><div class="text-button__text">Очистить</div></div>'
+        + '<div class="text-button"><div class="text-button__text">Применить</div></div>'
+        + '</div>');
+});
