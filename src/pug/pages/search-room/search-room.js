ff498cdfe9bb3db57f0724c9c2809332a4d2f5df
Nurvive <jiimbo689@gmail.com>
2022-01-10 00:26:53 +0700

    Обработал линтером

--- src/pug/pages/search-room/search-room.js
+++ src/pug/pages/search-room/search-room.js
@@ -1,25 +1,22 @@
-import './search-room-kit/search-room.scss'
-import 'jquery'
+import './search-room-kit/search-room-media.scss';
+import 'jquery';
 
 $().ready(() => {
-    let width = ($(window).width())
-    let moveObjects = $('li.search-room__item:not([data-move="no"])')
-    if (width < 900)
-        moveObjects.detach().appendTo('.small-menu__content')
+    let width = ($(window).width());
+    let moveObjects = $('li.search-room__item:not([data-move="no"])');
+    if (width < 900) moveObjects.detach().appendTo('.small-menu__content');
 
     $(window).resize(function () {
-        width = ($(this).width())
-        if (width < 900)
-            moveObjects.detach().appendTo('.small-menu__content')
+        width = ($(this).width());
+        if (width < 900) moveObjects.detach().appendTo('.small-menu__content');
         else {
-            moveObjects.detach().appendTo('.search-room__list')
+            moveObjects.detach().appendTo('.search-room__list');
         }
-    })
+    });
     $('.small-menu__content *:not(.dropdown__input-wrapper, .dropdown__open-button, .dropdown__input)').on('click', function (e) {
-        e.stopPropagation()
-    })
+        e.stopPropagation();
+    });
     $('.small-menu-title').on('click', function () {
-        $(this).parent().children(':not(.search-room__layout)').toggle(100)
-    })
-
-})
+        $(this).parent().children(':not(.search-room__layout)').toggle(100);
+    });
+});
