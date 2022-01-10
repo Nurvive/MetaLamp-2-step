4086a5a68e4854233ed3c0a75fa49a14bece7310
Nurvive <jiimbo689@gmail.com>
2022-01-10 16:39:53 +0700

    Добавил js префиксы и изменил let на const

--- src/pug/blocks/reservation/reservation.js
+++ src/pug/blocks/reservation/reservation.js
@@ -1,8 +1,8 @@
 import './reservation-kit/reservation-media.scss';
 
 $().ready(function () {
-    let $start = $('#reservation__date-dropdown_start');
-    let $end = $('#reservation__date-dropdown_end');
+    const $start = $('#reservation__date-dropdown_start');
+    const $end = $('#reservation__date-dropdown_end');
 
     $start.datepicker({
         onSelect: function (fd) {
--- src/pug/blocks/text-field/text-field.js
+++ src/pug/blocks/text-field/text-field.js
@@ -1,2 +1,1 @@
 import './text-field-kit/text-field.scss';
-
