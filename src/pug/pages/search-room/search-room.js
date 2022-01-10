48e6d6f36dd8b81e9bcfc0c98c97e85b9408ff67
Nurvive <jiimbo689@gmail.com>
2022-01-10 16:44:25 +0700

    Добавил $ префиксы

--- src/pug/pages/search-room/search-room.js
+++ src/pug/pages/search-room/search-room.js
@@ -3,14 +3,14 @@ import 'jquery';
 
 $().ready(() => {
     let width = ($(window).width());
-    let moveObjects = $('li.search-room__item:not([data-move="no"])');
-    if (width < 900) moveObjects.detach().appendTo('.small-menu__content');
+    let $moveObjects = $('li.search-room__item:not([data-move="no"])');
+    if (width < 900) $moveObjects.detach().appendTo('.small-menu__content');
 
     $(window).resize(function () {
         width = ($(this).width());
-        if (width < 900) moveObjects.detach().appendTo('.small-menu__content');
+        if (width < 900) $moveObjects.detach().appendTo('.small-menu__content');
         else {
-            moveObjects.detach().appendTo('.search-room__list');
+            $moveObjects.detach().appendTo('.search-room__list');
         }
     });
     $('.small-menu__content *:not(.dropdown__input-wrapper, .dropdown__open-button, .dropdown__input)').on('click', function (e) {
