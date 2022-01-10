d8879698de4f4057b6dfa1b6bf8a6b1a5d074cbd
Nurvive <jiimbo689@gmail.com>
2022-01-10 16:33:36 +0700

    Добавил знак $ переменным и изменил let на const

--- src/pug/blocks/header/header.js
+++ src/pug/blocks/header/header.js
@@ -14,11 +14,11 @@ $().ready(() => {
     $('.js-side-menu__menu-dropdown').on('click', function () {
         $(this).children('.js-side-menu__dropdown-item').toggle(400);
     });
-    let sideMenu = $('.js-side-menu');
-    sideMenu.on('touchstart', function (event) {
+    const $sideMenu = $('.js-side-menu');
+    $sideMenu.on('touchstart', function (event) {
         startingX = event.touches[0].clientX;
     });
-    sideMenu.on('touchmove', function (event) {
+    $sideMenu.on('touchmove', function (event) {
         let touch = event.touches[0];
         let change = startingX - touch.clientX;
         if (change < 0) {
@@ -27,7 +27,7 @@ $().ready(() => {
         this.style.left = '-' + change + 'px';
         event.preventDefault();
     });
-    sideMenu.on('touchend', function (event) {
+    $sideMenu.on('touchend', function (event) {
         let change = startingX - event.changedTouches[0].clientX;
         let threshold = window.screen.width / 3;
         if (change < threshold) {
