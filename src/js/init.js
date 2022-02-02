1a875fd06af3c36998658b330604eda21bc4dcb0
Nurvive <jiimbo689@gmail.com>
2022-02-02 17:50:48 +0700

    Сделал room-card классом #8

--- src/js/init.js
+++ src/js/init.js
@@ -3,6 +3,7 @@ import MaskedTextField from '../pug/blocks/masked-text-field/masked-text-field';
 import ExpandableCheckboxList from '../pug/blocks/expandable-checkbox-list/expandable-checkbox-list';
 import Pagination from '../pug/blocks/pagination/pagination';
 import RangeSlider from '../pug/blocks/range-slider/range-slider';
+import RoomCard from '../pug/blocks/room-card/room-card';
 // eslint-disable-next-line no-unused-vars
 const likeButton = new LikeButton();
 // eslint-disable-next-line no-unused-vars
@@ -13,3 +14,5 @@ const list = new ExpandableCheckboxList();
 const pagination = new Pagination();
 // eslint-disable-next-line no-unused-vars
 const rangeSlider = new RangeSlider();
+// eslint-disable-next-line no-unused-vars
+const roomCard = new RoomCard();
--- src/pug/blocks/room-card/room-card.js
+++ src/pug/blocks/room-card/room-card.js
@@ -2,9 +2,20 @@ import './room-card.scss';
 import 'slick-carousel';
 import 'slick-carousel/slick/slick.css';
 
-$('.js-room-card__top').slick({
-    dots: true,
-    prevArrow: '<button class="room-card__arrow room-card__arrow_prev fas fa-chevron-left"></button>',
-    nextArrow: '<button class="room-card__arrow room-card__arrow_next fas fa-chevron-right"></button>',
-    dotsClass: 'room-card__dots'
-});
+class RoomCard {
+    constructor() {
+        this.element = $('.js-room-card__top');
+        this.init();
+    }
+
+    init() {
+        this.element.slick({
+            dots: true,
+            prevArrow: '<button class="room-card__arrow room-card__arrow_prev fas fa-chevron-left"></button>',
+            nextArrow: '<button class="room-card__arrow room-card__arrow_next fas fa-chevron-right"></button>',
+            dotsClass: 'room-card__dots'
+        });
+    }
+}
+
+export default RoomCard;
