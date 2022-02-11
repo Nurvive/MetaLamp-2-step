ab1f95c9f9ca98ce3777dbca1c5711590a005e7c
Nurvive <jiimbo689@gmail.com>
2022-02-11 16:03:28 +0700

    Поправил инициализацию классов в init.js

--- src/js/init.js
+++ src/js/init.js
@@ -8,28 +8,12 @@ import Header from '../pug/blocks/header/header';
 import Canvas from '../pug/blocks/canvas/canvas';
 
 $(() => {
-    $('.js-like-button').each((_, element) => {
-        new LikeButton(element);
-    });
-    $('.js-masked-text-field input').each((_, element) => {
-        new MaskedTextField(element);
-    });
-    $('.js-expandable-checkbox__open-button').each((_, element) => {
-        new ExpandableCheckboxList(element);
-    });
-    $('.js-pagination').each((_, element) => {
-        new Pagination(element);
-    });
-    $('.js-range-slider').each((_, element) => {
-        new RangeSlider(element);
-    });
-    $('.room-card__top').each((_, element) => {
-        new RoomCard(element);
-    });
-    $('.js-header__menu-dropdown').each((_, element) => {
-        new Header(element);
-    });
-    $('.js-canvas__area').each((_, element) => {
-        new Canvas(element);
-    });
+    $('.js-like-button').each((_, element) => new LikeButton(element));
+    $('.js-masked-text-field input').each((_, element) => new MaskedTextField(element));
+    $('.js-expandable-checkbox__open-button').each((_, element) => new ExpandableCheckboxList(element));
+    $('.js-pagination').each((_, element) => new Pagination(element));
+    $('.js-range-slider').each((_, element) => new RangeSlider(element));
+    $('.room-card__top').each((_, element) => new RoomCard(element));
+    $('.js-header__menu-dropdown').each((_, element) => new Header(element));
+    $('.js-canvas__area').each((_, element) => new Canvas(element));
 });
