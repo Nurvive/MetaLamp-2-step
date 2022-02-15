905d7b479b48b43768c6b4b934d9280cb5a797a1
Nurvive <jiimbo689@gmail.com>
2022-02-15 17:45:26 +0700

    Добавил js-префикс #8

--- src/js/init.js
+++ src/js/init.js
@@ -13,7 +13,7 @@ $(() => {
     $('.js-expandable-checkbox__open-button').each((_, element) => new ExpandableCheckboxList(element));
     $('.js-pagination').each((_, element) => new Pagination(element));
     $('.js-range-slider').each((_, element) => new RangeSlider(element));
-    $('.room-card__top').each((_, element) => new RoomCard(element));
+    $('.js-room-card__top').each((_, element) => new RoomCard(element));
     $('.js-header__menu-dropdown').each((_, element) => new Header(element));
     $('.js-canvas__area').each((_, element) => new Canvas(element));
 });
