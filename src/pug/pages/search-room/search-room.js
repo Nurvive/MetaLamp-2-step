19b06a9513e89180c283fbe1e870686df076eb63
Nurvive <jiimbo689@gmail.com>
2022-02-18 19:22:03 +0700

    Поправил классы в search-room #13

--- src/pug/pages/search-room/search-room.js
+++ src/pug/pages/search-room/search-room.js
@@ -4,16 +4,16 @@ import 'ion-rangeslider/css/ion.rangeSlider.min.css';
 $().ready(() => {
     let width = ($(window).width());
     let $moveObjects = $('li.js-search-room__item:not([data-move="no"])');
-    if (width < 900) $moveObjects.detach().appendTo('.small-menu__content');
+    if (width < 900) $moveObjects.detach().appendTo('.search-room__small-menu-content');
 
     $(window).resize(function () {
         width = ($(this).width());
-        if (width < 900) $moveObjects.detach().appendTo('.small-menu__content');
+        if (width < 900) $moveObjects.detach().appendTo('.search-room__small-menu-content');
         else {
             $moveObjects.detach().appendTo('.search-room__list');
         }
     });
-    $('.js-small-menu-title').on('click', function () {
+    $('.js-search-room__small-menu > *[data-attr="small-menu-title"]').on('click', function () {
         $(this).parent().children(':not(.search-room__layout)').toggle(100);
     });
 });
