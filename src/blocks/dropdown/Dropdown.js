a1e620254cd201ff8e10f7ccd7b0372ade130c12
Nurvive <jiimbo689@gmail.com>
2022-04-20 21:20:33 +0700

    Поменял названия переменных в declOfNum #30

--- src/blocks/dropdown/Dropdown.js
+++ src/blocks/dropdown/Dropdown.js
@@ -228,15 +228,15 @@ class Dropdown {
     }
 
     static declOfNum(value, textForms) {
-        let bigValue = Math.abs(value) % 100;
-        let smallValue = value % 10;
-        if (bigValue > 10 && bigValue < 20) {
+        let hundredthPart = Math.abs(value) % 100;
+        let decimalPart = value % 10;
+        if (hundredthPart > 10 && hundredthPart < 20) {
             return textForms[2];
         }
-        if (smallValue > 1 && smallValue < 5) {
+        if (decimalPart > 1 && decimalPart < 5) {
             return textForms[1];
         }
-        if (smallValue === 1) {
+        if (decimalPart === 1) {
             return textForms[0];
         }
         return textForms[2];
